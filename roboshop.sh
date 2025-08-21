dbfd27cf7a3ccdba0ff75e4e46f83fa76758eab3
bana surekha reddy <surekhareddybana@gmail.com>
2025-08-21 15:44:26 +0530

    hello

---
+++ roboshop.sh
@@ -0,0 +1,21 @@
+#!\bin\bash
+
+AMI_id="ami-09c813fb71547fc4f"
+SG_ID="sg-0900091d833290dd7"
+INSTANCES=("mongodb" "redis" "mysql" "rabitmq" "catalogue" "shipping" "cart" "user" "payment" "dispatch" "frontend")
+ZONE_ID="Z10186462WRSH5GYHYLSN" #(in route53 we have zone id)
+DOMAIN_NAME="banasurekha.shop"
+
+for instances in ${INSTANCES[@]}
+do
+
+   INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro  --security-group-ids sg-0900091d833290dd7 --tag-specifations "ResourseType=instance,
+    Tags=[{key=Name, valiue=Test}]" --query "Instances[0].InstanceId" --output text
+   if [ $instance != "frontend" ]
+   then
+   IP=aws ec2 describe-instances  --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress"  --output text
+else
+IP=aws ec2 describe-instances  --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress"  --output text
+fi
+echo "$instance IP address: $IP"
+done
