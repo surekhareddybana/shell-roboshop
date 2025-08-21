9d4e6db2d2292d9d5c4db7c877f2f54c94c90488
bana surekha reddy <surekhareddybana@gmail.com>
2025-08-21 20:07:54 +0530

    hello

--- roboshop.sh
+++ roboshop.sh
@@ -9,8 +9,7 @@ DOMAIN_NAME="banasurekha.shop"
 for instance in ${INSTANCES[@]}
 do
 
-   INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro  --security-group-ids sg-0900091d833290dd7 --tag-specifations "ResourceType=instance,
-    Tags=[{key=Name, value=$instance}]" --query "Instances[0].InstanceId" --output text)
+   INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro  --security-group-ids sg-0900091d833290dd7 --tag-specifations "ResourceType=instance,Tags=[{key=Name, value=$instance}]" --query "Instances[0].InstanceId" --output text)
    if [ $instance != "frontend" ]
    then
       IP=$(aws ec2 describe-instances  --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress"  --output text)
