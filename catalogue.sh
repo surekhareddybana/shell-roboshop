7f13579db3c99f343cd32ae97d10231f2fea4572
bana surekha reddy <surekhareddybana@gmail.com>
2025-08-23 16:28:05 +0530

    hello

--- catalogue.sh
+++ catalogue.sh
@@ -82,5 +82,11 @@ cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
 dnf install mongodb-mongosh -y &>>$LOG_FILE
 VALIDATE $? "installing mongodb client"
 
+STATUS=$(mongosh --host mongodb.banasurekha.shop --eval 'db.getMongo().getDbNmames().indexof("catalogue")')
+if [ $STATUS -ne 1 ]
+then
+
 mongosh --host mongodb.banasurekha.shop </app/db/master-data.js &>>$LOG_FILE
 VALIDATE $? "loading data into mongodb"
+else 
+echo -e "data is already loaded $Y skipping $N"
